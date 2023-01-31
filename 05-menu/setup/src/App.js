3599133eb204e9dbf234585120181dbe2999ab5b
JacquelynSH <elpatchaembroidery@gmail.com>
2023-01-30 20:09:57 -0800

    Add Breakfast category

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -7,13 +7,24 @@ import menu from './data';
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([])
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category 
+    === category)
+    setMenuItems(newItems)
+  };
+
   return <main>
     <section className='menu section'>
       <div className='title'>
         <h2>our menu</h2>
         <div className='underline'></div>
       </div>
-      <Categories />
+      <Categories filterItems={filterItems}/>
       <Menu items={menuItems}/>
     </section>
   </main>
