66e9b912ec5268b6414890dedd1ac194583f0487
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-01 12:02:34 -0800

    Destructure and Display fetched data

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -27,7 +27,27 @@ function App() {
     );
   }
 
-  return <h2>tabs project setup</h2>
+  const { company, dates, duties, title } = jobs[value];
+  return <section className='section'>
+    <div className='title'>
+      <h2>experience</h2>
+      <div className='underline'></div>
+      {/* {btn container} */}
+      {/* {job info} */}
+      <article className='job-info'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty, index) => {
+          return (<div key={index} className='job-desc'>
+            <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+            <p>{duty}</p>
+          </div>
+          )
+        })}
+      </article>
+    </div>
+  </section>
 }
 
 export default App
