534d577ef79bdc877c0d0a0de5a826a8fd83a311
JacquelynSH <elpatchaembroidery@gmail.com>
2023-02-23 15:25:42 -0800

    Display data text with map function/parse count from string to num

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -7,10 +7,13 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('hello world');
+    setText(data);
+    
   }
 
   const paragraphCount = (e) => {
-    setCount(e.target.value)
+    setCount(parseInt(e.target.value))
+    console.log(typeof(count))
   }
 
 
@@ -27,7 +30,11 @@ function App() {
         <button type='submit' className='btn'>Generate</button>
       </form>
       <article className='lorem-text'>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A rem doloribus natus eligendi magnam optio, praesentium expedita cumque magni nemo repellat, impedit sit placeat earum delectus ea beatae voluptatibus. Autem?</p>
+        {text.map((item, index) => {
+          return (
+          <div key={index}>{item}</div>
+          )
+        })}
       </article>
     </section>
   )
